626d42c0597f561bd2599904ebfca8b8d6f3ff26
mathiuxd <matzap.ai@gmail.com>
2025-11-17 11:22:39 -0500

    base Arbol

--- src/main/java/com/universidad/modelo/ArbolABB.java
+++ src/main/java/com/universidad/modelo/ArbolABB.java
@@ -4,11 +4,33 @@ package com.universidad.modelo;
 import com.universidad.modelo.Estudiante;
 
 public class ArbolABB {
-    
+
     private NodoABB raiz;
 
     public ArbolABB() {
         this.raiz = null;
+    }
+
+    public NodoABB getRaiz() {
+        return raiz;
+    }
+
+    public void insertar(Estudiante est) {
+        raiz = insertarRec(raiz, est);
+    }
+
+    // Pendiente
+    private NodoABB insertarRec(NodoABB nodo, Estudiante est) {
+        return nodo;
+    }
+
+    public Estudiante buscar(String matricula) {
+        return buscarRec(raiz, matricula);
+    }
+
+    // Pendiente
+    private Estudiante buscarRec(NodoABB nodo, String matricula) {
+        return null;
     }
 
 }
--- src/main/java/com/universidad/negocio/SistemaRegistro.java
+++ src/main/java/com/universidad/negocio/SistemaRegistro.java
@@ -1,14 +1,5 @@
 package com.universidad.negocio;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author jeto
- */
 public class SistemaRegistro {
     
 }
